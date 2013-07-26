6377198889a382ff81d3fb4048265294e81706c8
Billy Shih <me@billyshih.com>
2013-07-25 22:33:35 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Festivalize::Application.config.secret_key_base = 'db732e6ff3c4b4144b514d94936c62dbcbf5183ac4e7d23bfa388ea282de34dbcb831d6c0b43426d936c09d927a26c28e0d7c4a47adc2da8c4763897b1c8164a'
