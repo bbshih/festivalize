b40d4872b907d9d768a8b725cc6af3999b070ca9
Billy Shih <me@billyshih.com>
2013-07-29 22:37:32 -0700

    Added editing

--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -1,7 +1,8 @@
 class FestivalsController < ApplicationController
-  before_filter :find_festival, only: [:show]
+  before_filter :find_festival, only: [:show, :edit, :update]
 
   def index
+    @festivals = Festival.all
   end
 
   def new
@@ -12,6 +13,20 @@ class FestivalsController < ApplicationController
     @festival = Festival.create(festival_params)
     if @festival.save
       redirect_to @festival, notice: "Festival added"
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    binding.pry
+    if @festival.update_attributes(festival_params)
+      redirect_to @festival, notice: "Changes saved"
+    else
+      flash[:alert] = "Changes failed to save"
+      render action: :edit
     end
   end
 
